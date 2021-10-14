36ab58631fe3bc4a28cbdfafd476734130e83975
Tani Kaye <1tanikaye@gmail.com>
2021-10-14 16:58:26 -0400

    commit tree 14658ae72db2c739d3c877b9e7d0b8c38572f589

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(sentence => {
+    const sentenceArray = sentence.split(' ');
+
+    return sentenceArray.map(word => {
+     return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(' ')
+  })
 }
